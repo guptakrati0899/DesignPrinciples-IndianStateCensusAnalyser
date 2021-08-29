7492b3024674ca83a8a5453650035774ba60d57f
KratiGupta <kratigupta8545@gmail.com>
2021-08-29 23:39:48 +0530

    Refactoored the program for Matching the number of records

--- test/java/com/indianstatecensus/StateAnalyserTest.java
+++ test/java/com/indianstatecensus/StateAnalyserTest.java
@@ -5,15 +5,30 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+
 public class StateAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\Asus\\IdeaProjects\\IndianStateCensusAnalyser\\src\\test\\resources\\IndianStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assertions.assertEquals(29, numOfRecords);
+        } catch (CensusAnalyserException e) {
+        } catch (CSVBuilderException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void givenIndianStateCSV_shouldReturnExactCount() {
         try {
             StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfStateCode = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-            Assertions.assertEquals(29, numOfStateCode);
+            int numOfStateCensus = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assertions.assertEquals(29, numOfStateCensus);
         } catch (CensusAnalyserException e) {
             Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         } catch (CSVBuilderException e) {
